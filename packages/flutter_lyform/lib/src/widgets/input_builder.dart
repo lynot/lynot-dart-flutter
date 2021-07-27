02db6a8cc1952d46e13a9bec4e30d6e610ae6f27
Tony Raul Blanco Fernandez <70nybl4nc0@gmail.com>
2021-07-27 15:33:34 -0400

    fix(flutter_lyform): remove generic type of InputBlocBuilder

--- packages/flutter_lyform/lib/src/widgets/input_builder.dart
+++ packages/flutter_lyform/lib/src/widgets/input_builder.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lyform/lyform.dart';
 
-class InputBlocBuilder<T, E> extends StatelessWidget {
+class InputBlocBuilder<T> extends StatelessWidget {
   final Widget Function(BuildContext, InputBlocState<T>) builder;
 
   final InputBloc<T> bloc;
