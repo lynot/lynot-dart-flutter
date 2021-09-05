e3a901372f93b709098bd2a9bfb68cd96318dc37
Tony Raul Blanco Fernandez <70nybl4nc0@gmail.com>
2021-09-05 12:07:07 -0400

    fix(lyform): fix validation on submit

--- packages/lyform/lib/src/form/form_bloc.dart
+++ packages/lyform/lib/src/form/form_bloc.dart
@@ -19,7 +19,7 @@ abstract class FormBloc<D, E> extends Bloc<FormBlocEvent, FormBlocState<D, E>> {
   Future<FormBlocState<D, E>> onSubmmit();
 
   bool isValid() {
-    return inputs.every((input) => input.isValid || !input.isPure);
+    return inputs.every((input) => input.isValid);
   }
 
   bool isPure() {
