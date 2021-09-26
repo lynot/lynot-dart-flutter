951bb5a2f0492b42008d025442b0112ebfe9141f
leynier <leynier41@gmail.com>
2021-09-26 14:10:18 -0400

    feat: implement more string validators

--- packages/lyform_validators/lib/src/string_validators.dart
+++ packages/lyform_validators/lib/src/string_validators.dart
@@ -107,53 +107,111 @@ class StringValidator {
     return (value) => !validators.isIP(value, version) ? errorMessage : null;
   }
 
-  // /// check if the string [value] is a fully qualified domain name (e.g. domain.com).
-  // ///
-  // /// * [requireTld] sets if TLD is required
-  // /// * [allowUnderscore] sets if underscores are allowed
-  // bool isFQDN(
-  //   String value, {
-  //   bool requireTld = true,
-  //   bool allowUnderscores = false,
-  // }) {}
+  /// check if the string [value] is a fully qualified domain name (e.g. domain.com).
+  ///
+  /// * [requireTld] sets if TLD is required
+  /// * [allowUnderscore] sets if underscores are allowed
+  static InputValidator<String?> isFQDN({
+    required String errorMessage,
+    bool requireTld = true,
+    bool allowUnderscores = false,
+  }) {
+    return (value) =>
+        value == null || !validators.isFQDN(value) ? errorMessage : null;
+  }
 
-  // /// check if the string [value] contains only letters (a-zA-Z).
-  // bool isAlpha(value) {}
+  /// check if the string [value] contains only letters (a-zA-Z).
+  static InputValidator<String?> isAlpha({
+    required String errorMessage,
+  }) {
+    return (value) =>
+        value == null || !validators.isAlpha(value) ? errorMessage : null;
+  }
 
-  // /// check if the string [value] contains only numbers
-  // bool isNumeric(value) {}
+  /// check if the string [value] contains only numbers
+  static InputValidator<String?> isNumeric({
+    required String errorMessage,
+  }) {
+    return (value) =>
+        value == null || !validators.isNumeric(value) ? errorMessage : null;
+  }
 
-  // /// check if the string [value] contains only letters and numbers
-  // bool isAlphanumeric(value) {}
+  /// check if the string [value] contains only letters and numbers
+  static InputValidator<String?> isAlphanumeric({
+    required String errorMessage,
+  }) {
+    return (value) => value == null || !validators.isAlphanumeric(value)
+        ? errorMessage
+        : null;
+  }
 
-  // /// check if a string [value] is base64 encoded
-  // bool isBase64(value) {}
+  /// check if a string [value] is base64 encoded
+  static InputValidator<String?> isBase64({
+    required String errorMessage,
+  }) {
+    return (value) =>
+        value == null || !validators.isBase64(value) ? errorMessage : null;
+  }
 
-  // /// check if the string [value] is an integer
-  // bool isInt(value) {}
+  /// check if the string [value] is an integer
+  static InputValidator<String?> isInt({
+    required String errorMessage,
+  }) {
+    return (value) =>
+        value == null || !validators.isInt(value) ? errorMessage : null;
+  }
 
-  // /// check if the string [value] is a float
-  // bool isFloat(value) {}
+  /// check if the string [value] is a float
+  static InputValidator<String?> isFloat({
+    required String errorMessage,
+  }) {
+    return (value) =>
+        value == null || !validators.isFloat(value) ? errorMessage : null;
+  }
 
-  // /// check if the string  [value]is a hexadecimal number
-  // bool isHexadecimal(value) {}
+  /// check if the string  [value]is a hexadecimal number
+  static InputValidator<String?> isHexadecimal({
+    required String errorMessage,
+  }) {
+    return (value) =>
+        value == null || !validators.isHexadecimal(value) ? errorMessage : null;
+  }
 
-  // /// check if the string [value] is a hexadecimal color
-  // bool isHexColor(value) {}
+  /// check if the string [value] is a hexadecimal color
+  static InputValidator<String?> isHexColor({
+    required String errorMessage,
+  }) {
+    return (value) =>
+        value == null || !validators.isHexColor(value) ? errorMessage : null;
+  }
 
-  // /// check if the string [value] is lowercase
-  // bool isLowercase(value) {}
+  /// check if the string [value] is lowercase
+  static InputValidator<String?> isLowercase({
+    required String errorMessage,
+  }) {
+    return (value) =>
+        value == null || !validators.isLowercase(value) ? errorMessage : null;
+  }
 
-  // /// check if the string [value] is uppercase
-  // bool isUppercase(value) {}
+  /// check if the string [value] is uppercase
+  static InputValidator<String?> isUppercase({
+    required String errorMessage,
+  }) {
+    return (value) =>
+        value == null || !validators.isUppercase(value) ? errorMessage : null;
+  }
 
   // /// check if the string [value] is a number that's divisible by another
   // ///
   // /// [n] is a String or an int.
   // bool isDivisibleBy(String value, n) {}
 
-  // /// check if the string [value] is null
-  // bool isNull(String? value) {}
+  /// check if the string [value] is null
+  static InputValidator<String?> isNull({
+    required String errorMessage,
+  }) {
+    return (value) => value != null ? errorMessage : null;
+  }
 
   // /// check if the length of the string [value] falls in a range
   // bool isLength(String value, int min, [int? max]) {}
@@ -164,8 +222,13 @@ class StringValidator {
   // /// check if the string is a UUID (version 3, 4 or 5).
   // bool isUUID(String? value, [version]) {}
 
-  // /// check if the string is a date
-  // bool isDate(value) {}
+  /// check if the string is a date
+  static InputValidator<String?> isDate({
+    required String errorMessage,
+  }) {
+    return (value) =>
+        value == null || !validators.isDate(value) ? errorMessage : null;
+  }
 
   // /// check if the string is a date that's after the specified date
   // ///
@@ -180,35 +243,82 @@ class StringValidator {
   // /// check if the string is in a array of allowed values
   // bool? isIn(String? value, values) {}
 
-  // /// check if the string is a credit card
-  // bool isCreditCard(value) {}
+  /// check if the string is a credit card
+  static InputValidator<String?> isCreditCard({
+    required String errorMessage,
+  }) {
+    return (value) =>
+        value == null || !validators.isCreditCard(value) ? errorMessage : null;
+  }
 
   // /// check if the string is an ISBN (version 10 or 13)
   // bool isISBN(String? value, [version]) {}
 
-  // /// check if the string is valid JSON
-  // bool isJSON(value) {}
+  /// check if the string is valid JSON
+  static InputValidator<String?> isJSON({
+    required String errorMessage,
+  }) {
+    return (value) =>
+        value == null || !validators.isJSON(value) ? errorMessage : null;
+  }
 
-  // /// check if the string contains one or more multibyte chars
-  // bool isMultibyte(value) {}
+  /// check if the string contains one or more multibyte chars
+  static InputValidator<String?> isMultibyte({
+    required String errorMessage,
+  }) {
+    return (value) =>
+        value == null || !validators.isMultibyte(value) ? errorMessage : null;
+  }
 
-  // /// check if the string contains ASCII chars only
-  // bool isAscii(value) {}
+  /// check if the string contains ASCII chars only
+  static InputValidator<String?> isAscii({
+    required String errorMessage,
+  }) {
+    return (value) =>
+        value == null || !validators.isAscii(value) ? errorMessage : null;
+  }
 
-  // /// check if the string contains any full-width chars
-  // bool isFullWidth(value) {}
+  /// check if the string contains any full-width chars
+  static InputValidator<String?> isFullWidth({
+    required String errorMessage,
+  }) {
+    return (value) =>
+        value == null || !validators.isFullWidth(value) ? errorMessage : null;
+  }
 
-  // /// check if the string contains any half-width chars
-  // bool isHalfWidth(value) {}
+  /// check if the string contains any half-width chars
+  static InputValidator<String?> isHalfWidth({
+    required String errorMessage,
+  }) {
+    return (value) =>
+        value == null || !validators.isHalfWidth(value) ? errorMessage : null;
+  }
 
-  // /// check if the string contains a mixture of full and half-width chars
-  // bool isVariableWidth(value) {}
+  /// check if the string contains a mixture of full and half-width chars
+  static InputValidator<String?> isVariableWidth({
+    required String errorMessage,
+  }) {
+    return (value) => value == null || !validators.isVariableWidth(value)
+        ? errorMessage
+        : null;
+  }
 
-  // /// check if the string contains any surrogate pairs chars
-  // bool isSurrogatePair(value) {}
+  /// check if the string contains any surrogate pairs chars
+  static InputValidator<String?> isSurrogatePair({
+    required String errorMessage,
+  }) {
+    return (value) => value == null || !validators.isSurrogatePair(value)
+        ? errorMessage
+        : null;
+  }
 
-  // /// check if the string is a valid hex-encoded representation of a MongoDB ObjectId
-  // bool isMongoId(value) {}
+  /// check if the string is a valid hex-encoded representation of a MongoDB ObjectId
+  static InputValidator<String?> isMongoId({
+    required String errorMessage,
+  }) {
+    return (value) =>
+        value == null || !validators.isMongoId(value) ? errorMessage : null;
+  }
 
   // bool isPostalCode(String? text, String locale, {bool orElse()?}) {}
 }
