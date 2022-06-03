dedcfb8bbc5c624c2d18114bb4aacc4d6287d1aa
Yeikel Uriarte Arteaga <uyeikel@gmail.com>
2022-06-03 15:41:53 -0400

    docs(lyform_validators): add docs in `IterableNotEmtpy` and `Required`

--- packages/lyform_validators/lib/src/general_validators.dart
+++ packages/lyform_validators/lib/src/general_validators.dart
@@ -1,6 +1,14 @@
 import 'package:lyform/lyform.dart';
 
+/// {@template required_validators}
+/// Type validation required.
+///
+/// If the provided `value` is `null` then it will be invalid and
+/// will return the error [message], otherwise it returns null.
+///
+/// {@endtemplate}
 class Required<T> extends Validator<T> {
+  /// {@macro required_validators}
   Required(String message) : super(message);
 
   @override
