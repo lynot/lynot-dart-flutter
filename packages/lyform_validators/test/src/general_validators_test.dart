480fa8e55ee0d98fee994b88fa0a637c77b62693
Yeikel Uriarte Arteaga <uyeikel@gmail.com>
2022-06-03 15:42:13 -0400

    test(lyform_validators): add tests in `IterableNotEmtpy` and `Required`

---
+++ packages/lyform_validators/test/src/general_validators_test.dart
@@ -0,0 +1,20 @@
+import 'package:lyform_validators/src/general_validators.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('General Validators', () {
+    group('Required', () {
+      test('should return `null` when the `value` is not null', () {
+        final validation = Required<int>('Must be required');
+        final result = validation(15);
+        expect(result, isNull);
+      });
+
+      test('should return a `message` when the `value` is null', () {
+        final validation = Required<int?>('Must be required');
+        final result = validation(null);
+        expect(result, 'Must be required');
+      });
+    });
+  });
+}
